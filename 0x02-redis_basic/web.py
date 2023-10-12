ef7107f57a3c7cb0a20387eb81722a91574b9888
tomi-kid <tomiketema209@gmail.com>
2023-10-12 14:21:11 +0300

    bakend-storage

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+"""web module
+"""
+import requests
+import redis
+from functools import wraps
+from typing import Callable
+
+_redis = redis.Redis()
+
+
+def count_requests(method: Callable) -> Callable:
+    """count_requests function
+
+    Args:
+        method (Callable): method
+
+    Returns:
+        Callable: wrapper
+    """
+    @wraps(method)
+    def wrapper(*args, **kwargs):
+        """wrapper function
+
+        Returns:
+            [type]: wrapper
+        """
+        url = args[0]
+        cached = _redis.get(f"cached:{url}")
+        if cached:
+            return cached.decode("utf-8")
+        response = method(*args, **kwargs)
+        _redis.incr(f"count:{url}")
+        _redis.setex(f"cached:{url}", 10, response)
+        return response
+    return wrapper
+
+
+@count_requests
+def get_page(url: str) -> str:
+    """get_page function
+
+    Args:
+        url (str): url
+
+    Returns:
+        str: response
+    """
+    response = requests.get(url)
+    return response.text
+
+
+if __name__ == '__main__':
+    get_page("http://google.com")
